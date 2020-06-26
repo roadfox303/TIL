0e5745f86624bb05527e2b9e2aeb4ea997fd664b
takamori nambu <roadfox303@gmail.com>
2020-06-26 17:46:29 +0900

    Cherry本 2章

---
+++ Ruby/Cherry_book/sample.rb
@@ -0,0 +1,13 @@
+puts 1 + 2
+
+a = 'Hello World!'
+puts a
+
+b = 'こんにちは'
+puts b
+
+puts "こんにちは\nさようなら"
+puts "あいうえお\rかきくけこ"
+puts "さしすせそ\tたちつてと"
+puts "こんにちは\\nさようなら"
+puts 'He said "Don\'t speak"'
