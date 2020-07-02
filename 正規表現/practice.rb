db6871d351dd7504aee7610f0af3d0d524c58a5f
takamori nambu <roadfox303@gmail.com>
2020-07-02 19:03:57 +0900

    手と目で覚える正規表現入門・その２

---
+++ 正規表現/practice.rb
@@ -0,0 +1,19 @@
+text = <<-TEXT
+名前：伊藤淳一
+電話：03-1234-5678
+住所：兵庫県西脇市板波町1-2-3
+TEXT
+puts text.scan /\d\d-\d\d\d\d-\d\d\d\d/
+
+html = <<-HTML
+<select name="game_console">
+<option value="none"></option>
+<option value="wii_u" selected>Wii U</option>
+<option value="ps4">プレステ4</option>
+<option value="gb">ゲームボーイ</option>
+</select>
+HTML
+
+replaced = html.gsub(/<option value="(\w+)"(?: selected)?>(.*)<\/option>/, '\1,\2')
+
+puts replaced
