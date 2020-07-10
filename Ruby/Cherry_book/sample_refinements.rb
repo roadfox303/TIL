ae736d169637a5c095c872e1d9654bcd84dad5ba
takamori nambu <roadfox303@gmail.com>
2020-07-10 14:17:29 +0900

    Cherry本 8章

---
+++ Ruby/Cherry_book/sample_refinements.rb
@@ -0,0 +1,26 @@
+module StringShuffle
+  refine String do
+    def shuffle
+      chars.shuffle.join
+    end
+  end
+end
+
+'Alice'.shuffle
+
+class User
+  using StringShuffle
+
+  def initialize(name)
+    @name = name
+  end
+
+  def shuffled_name
+    @name.shuffle
+  end
+end
+
+user = User.new('Alice')
+user.shuffled_name
+
+'Alice'.shuffle
